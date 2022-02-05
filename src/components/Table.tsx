8dcb27d452860806956e6826599f250c353aba28
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-02-05 20:53:06 +0200

    Edit Table.tsx: add edit and delete buttons for the table rows, add handleDelete and handleEditing functions

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -6,10 +6,27 @@ interface Props {
     firstName: string;
     lastName: string;
     age: number;
+    id: string;
   }[];
+  addData: React.Dispatch<
+    React.SetStateAction<
+      {
+        firstName: string;
+        lastName: string;
+        age: number;
+        id: string;
+      }[]
+    >
+  >;
 }
 
-export const Table: React.FC<Props> = ({ data }) => {
+export const Table: React.FC<Props> = ({ data, addData }) => {
+  const handleEditing = (id: string): void => {};
+
+  const handleDelete = (id: string): void => {
+    addData([...data.filter((info) => info.id !== id)]);
+  };
+
   return (
     <div className="container">
       <table>
@@ -18,6 +35,7 @@ export const Table: React.FC<Props> = ({ data }) => {
             <th>Etunimi</th>
             <th>Sukunimi</th>
             <th>Ikä</th>
+            <th>Muokkaa</th>
           </tr>
         </thead>
         <tbody>
@@ -26,6 +44,20 @@ export const Table: React.FC<Props> = ({ data }) => {
               <td>{info.firstName}</td>
               <td>{info.lastName}</td>
               <td>{info.age}</td>
+              <td>
+                <button
+                  className="tableButton"
+                  onClick={() => handleEditing(info.id)}
+                >
+                  <span className="material-icons">edit</span>
+                </button>{" "}
+                <button
+                  className="tableButton"
+                  onClick={() => handleDelete(info.id)}
+                >
+                  <span className="material-icons">delete</span>
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
