1f8b33c0b719c9fd7408842f4416ce22c73c36e8
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-02-05 14:06:49 +0200

    Edit Form.tsx: edit the input fields, add onSubmit and onChange functions

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
-export const Form: React.FC = () => {
+interface Props {
+  addData: (arg: object) => void;
+}
+
+export const Form: React.FC<Props> = ({ addData }) => {
+  const [values, setValues] = useState({});
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(values);
+    addData(values);
+  };
+
   return (
-    <form>
+    <form className="form" onSubmit={onSubmit}>
       <h2>Lisää henkilö</h2>
-      <label>
-        Etunimi:
-        <input type="text"></input>
-      </label>
-      <label>
-        Sukunimi:
-        <input></input>
-      </label>
-      <label>
-        Ikä:
-        <input></input>
-      </label>
+      <div>
+        <label>
+          Etunimi:
+          <input name="firstname" type="text" onChange={onChange}></input>
+        </label>
+      </div>
+      <div>
+        <label>
+          Sukunimi:
+          <input name="lastname" type="text" onChange={onChange}></input>
+        </label>
+      </div>
+      <div>
+        <label>
+          Ikä:
+          <input name="age" type="number" onChange={onChange}></input>
+        </label>
+      </div>
       <button type="submit">Lisää</button>
     </form>
   );
