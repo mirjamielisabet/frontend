530bcf3ac2b784acbb1db9ba86a182e6c771e7f6
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-02-05 20:50:55 +0200

    Edit Form.tsx: modify the structure of the form, add property id

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -10,6 +10,7 @@ interface IProps {
         firstName: string;
         lastName: string;
         age: number;
+        id: string;
       }[]
     >
   >;
@@ -28,13 +29,13 @@ export const Form: React.FC<IProps> = ({ data, addData }) => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-
     addData([
       ...data,
       {
         firstName: values.firstName,
         lastName: values.lastName,
         age: parseInt(values.age),
+        id: Date.now().toString(36) + Math.random().toString(36),
       },
     ]);
 
@@ -48,46 +49,42 @@ export const Form: React.FC<IProps> = ({ data, addData }) => {
   return (
     <form className="container" onSubmit={handleSubmit}>
       <h2>Lisää henkilö</h2>
-      <p>* = pakollinen kenttä</p>
+      <p className="smalltext">* = pakollinen kenttä</p>
       <div>
-        <label>
-          Etunimi*:
-          <input
-            name="firstName"
-            type="text"
-            value={values.firstName}
-            onChange={handleChange}
-            required
-          ></input>
-        </label>
+        <label>Etunimi*:</label>
+        <input
+          name="firstName"
+          type="text"
+          value={values.firstName}
+          onChange={handleChange}
+          required
+        ></input>
       </div>
       <div>
-        <label>
-          Sukunimi*:
-          <input
-            name="lastName"
-            type="text"
-            value={values.lastName}
-            onChange={handleChange}
-            required
-          ></input>
-        </label>
+        <label>Sukunimi*:</label>
+        <input
+          name="lastName"
+          type="text"
+          value={values.lastName}
+          onChange={handleChange}
+          required
+        ></input>
       </div>
       <div>
-        <label>
-          Ikä*:
-          <input
-            name="age"
-            type="number"
-            value={values.age}
-            onChange={handleChange}
-            min="0"
-            max="150"
-            required
-          ></input>
-        </label>
+        <label>Ikä*:</label>
+        <input
+          name="age"
+          type="number"
+          value={values.age}
+          onChange={handleChange}
+          min="0"
+          max="150"
+          required
+        ></input>
       </div>
-      <button type="submit">Lisää</button>
+      <button className="button" type="submit">
+        Lisää
+      </button>
     </form>
   );
 };
