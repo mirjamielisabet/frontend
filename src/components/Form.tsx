02d5c4f84eabcd56849edb3149897344c1815406
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-02-05 16:27:56 +0200

    Edit Form.tsx: modify function names: onChange to handleChange and onSubmit to handleSubmit. Edit the attributes of the input fields and pass the inputted data to the App.tsx

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,42 +1,90 @@
 import React, { useState } from "react";
 import "../App.css";
+import { IState as Props } from "../App";
 
-interface Props {
-  addData: (arg: object) => void;
+interface IProps {
+  data: Props["people"];
+  addData: React.Dispatch<
+    React.SetStateAction<
+      {
+        firstName: string;
+        lastName: string;
+        age: number;
+      }[]
+    >
+  >;
 }
 
-export const Form: React.FC<Props> = ({ addData }) => {
-  const [values, setValues] = useState({});
+export const Form: React.FC<IProps> = ({ data, addData }) => {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    age: "",
+  });
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     setValues({ ...values, [event.target.name]: event.target.value });
   };
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-    console.log(values);
-    addData(values);
+
+    addData([
+      ...data,
+      {
+        firstName: values.firstName,
+        lastName: values.lastName,
+        age: parseInt(values.age),
+      },
+    ]);
+
+    setValues({
+      firstName: "",
+      lastName: "",
+      age: "",
+    });
   };
 
   return (
-    <form className="form" onSubmit={onSubmit}>
+    <form className="container" onSubmit={handleSubmit}>
       <h2>Lisää henkilö</h2>
+      <p>* = pakollinen kenttä</p>
       <div>
         <label>
-          Etunimi:
-          <input name="firstname" type="text" onChange={onChange}></input>
+          Etunimi*:
+          <input
+            name="firstName"
+            type="text"
+            value={values.firstName}
+            onChange={handleChange}
+            required
+          ></input>
         </label>
       </div>
       <div>
         <label>
-          Sukunimi:
-          <input name="lastname" type="text" onChange={onChange}></input>
+          Sukunimi*:
+          <input
+            name="lastName"
+            type="text"
+            value={values.lastName}
+            onChange={handleChange}
+            required
+          ></input>
         </label>
       </div>
       <div>
         <label>
-          Ikä:
-          <input name="age" type="number" onChange={onChange}></input>
+          Ikä*:
+          <input
+            name="age"
+            type="number"
+            value={values.age}
+            onChange={handleChange}
+            min="0"
+            max="150"
+            required
+          ></input>
         </label>
       </div>
       <button type="submit">Lisää</button>
