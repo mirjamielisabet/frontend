04c2214bea1064d6f0253ee1ec9a09c7bc75c480
Mirjami Laiho <mirjami.laiho@tuni.fi>
2022-02-05 16:31:43 +0200

    Edit App.tsx: create interface IState that contains the people object, save the inputted data to the state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Form } from "./components/Form";
+import { Table } from "./components/Table";
 
-function App() {
+export interface IState {
+  people: {
+    firstName: string;
+    lastName: string;
+    age: number;
+  }[];
+}
+
+const App: React.FC = () => {
+  const [data, addData] = useState<IState["people"]>([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form data={data} addData={addData} />
+      <br />
+      <Table data={data} />
     </div>
   );
-}
+};
 
 export default App;
